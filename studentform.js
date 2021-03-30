c9398dd50a6ce0d219edc6bd09a290b7059ad435
Bashar Adeed <adeed7222@gmail.com>
2021-03-30 23:54:56 +0530

    student_Registration

--- studentform.js
+++ studentform.js
@@ -1,123 +1,54 @@
 import React from 'react';
 
-import $ from 'jquery';
-
-export default class StudentForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        Pid: "",
-        Email: "",
-        Dept_Code: "",  
-        Course_Name: "",
-        CGPA: "",
-        Semester: "",
-        Description: "",
-        Date_of_birth: ""
+export default class studentForm extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      Pid: "",
+      Email: "",
+      Dept_Code: "",  
+      Course_Name: "",
+      CGPA: "",
+      Semester: "",
+      Description: "",
+      Date_of_birth: ""
     }
   }
 
-  updatePid(event) {
-    this.setState({
-      Pid: event.target.value 
-    });
-  }
-
-  updateEmail(event) {
-    this.setState({
-      Email: event.target.value 
-    });
-  }
-
-  updateDeptCode(event) {
-    this.setState({
-      DeptCode: event.target.value 
-    });
-  }
-
-  updateCourseName(event) {
-    this.setState({
-      CourseName: event.target.value 
-    });
-  }
-
-  updateCGPA(event) {
-    this.setState({
-      CGPA: event.target.value 
-    });
-  }
-
-  updateSemester(event) {
-    this.setState({
-      Semester: event.target.value 
-    });
-  }
-
-  updateDesciption(event) {
-    this.setState({
-      Description: event.target.value 
-    });
-  }
-
-  updateDateOfBirth(event) {
-    this.setState({
-      DateOfBirth: event.target.value 
-    });
-  }
-
-  save() {
-    var context = this;
-
-    $.ajax({
-      url: "http://127.0.0.1:5000/addstudent",
-      method: "POST",
-      data: {
-        Pid: context.state.Pid,
-        Email: context.state.Email,
-        Dept_Code: context.state.DeptCode,
-        Course_Name: context.state.CourseName,
-        CGPA: context.state.CGPA,
-        Semester: context.state.Semester,
-        Description: context.state.Description,
-        Date_of_birth: context.state.DateOfBirth
+  submit()
+  {
+    let url="http://127.0.0.1:5000/addstudent";
+    let data=this.state;
+    fetch(url,{
+      method:'POST',
+      headers:{
+        "Content-Type":"application/json",
+        "Accept":"application/json"
       },
-      success: function(response) {
-        alert("Successfully saved record!");
-      },
-      error: function(response) {
-        alert("Error in saving record!");
-      }
-    });
+      body:JSON.stringify(data)
+    }).then((result)=>{
+      result.json().then((resp)=>{
+        console.warn("resp".resp)
+      })
+    })
   }
 
   render() {
+
     return (
       <div>
-        PID:
-        <input type="text" value={this.state.Pid} onChange={this.updatePid.bind(this)} />
-        Email:
-        <input type="email" value={this.state.Email} onChange={this.updateEmail.bind(this)} />
-        Department Code:
-        <input type="text" value={this.state.DeptCode} onChange={this.updateDeptCode.bind(this)} />
-        Course Name:
-        <input type="text" value={this.state.CourseName} onChange={this.updateCourseName.bind(this)} />
-        CGPA:
-        <input type="number" step="0.01" value={this.state.CGPA} onChange={this.updateCGPA.bind(this)} />
-        Semester:
-        <input type="number" value={this.state.Semester} onChange={this.updateSemester.bind(this)} />
-        Description:
-        <input type="text" value={this.state.Description} onChange={this.updateDesciption.bind(this)} />
-        Date of Birth:
-        <input type="date" value={this.state.DateOfBirth} onChange={this.updateDateOfBirth.bind(this)} />
-        <hr/>
+        <h1> student Registration Form</h1>
+        <input type="text" value={this.state.Pid} name="Pid" onChange={(data )=>{this.setState({Pid:data.target.value}) }} /> <br /> <br />
+        <input type="email" value={this.state.Email} name="Email" onChange={(data )=>{ this.setState({Email:data.target.value}) }} /> <br /> <br />
+        <input type="text" value={this.state.Dept_Code} name="Dept_code" onChange={(data )=>{this.setState({Dept_Code:data.target.value}) }} /> <br /> <br />
+        <input type="text" value={this.state.Course_Name} name="Course_Name" onChange={(data )=>{this.setState({Course_Name:data.target.value}) }} /> <br /> <br />
+        <input type="number" step="0.01" value={this.state.CGPA} name="CGPA" onChange={(data )=>{this.setState({CGPA:data.target.value}) }} /> <br /> <br />
+        <input type="number" value={this.state.Semester} name="Semester" onChange={(data )=>{this.setState({Semester:data.target.value}) }} /> <br /> <br />
+        <input type="text" value={this.state.Description} name="Description" onChange={(data )=>{this.setState({Description:data.target.value}) }} /> <br /> <br />
+        <input type="date" value={this.state.Date_of_birth} name="Date_of_birth" onChange={(data )=>{this.setState({Date_of_birth:data.target.value}) }} /> <br /> <br />
 
-        <button onClick={this.save.bind(this)}>
-          Save
-        </button>
+        <button onClick={()=>{this.submit()}} >Submit</button>
       </div>
-    );
+    )
   }
 }
-
-export const element=<StudentForm></StudentForm>;
