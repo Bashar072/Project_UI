a79858274be5e87e8164bd602a075c6fdfad7adc
Bashar Adeed <adeed7222@gmail.com>
2021-04-01 08:50:01 +0530

    UI

--- studentform.js
+++ studentform.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Form, Input, InputNumber, Button, DatePicker, message } from 'antd';
+import axios from 'axios'
 
 export default class studentForm extends React.Component {
   constructor() {
@@ -38,17 +40,32 @@ export default class studentForm extends React.Component {
     return (
       <div>
         <h1> student Registration Form</h1>
-        <input type="text" value={this.state.Pid} name="Pid" onChange={(data )=>{this.setState({Pid:data.target.value}) }} /> <br /> <br />
-        <input type="email" value={this.state.Email} name="Email" onChange={(data )=>{ this.setState({Email:data.target.value}) }} /> <br /> <br />
-        <input type="text" value={this.state.Dept_Code} name="Dept_code" onChange={(data )=>{this.setState({Dept_Code:data.target.value}) }} /> <br /> <br />
-        <input type="text" value={this.state.Course_Name} name="Course_Name" onChange={(data )=>{this.setState({Course_Name:data.target.value}) }} /> <br /> <br />
-        <input type="number" step="0.01" value={this.state.CGPA} name="CGPA" onChange={(data )=>{this.setState({CGPA:data.target.value}) }} /> <br /> <br />
-        <input type="number" value={this.state.Semester} name="Semester" onChange={(data )=>{this.setState({Semester:data.target.value}) }} /> <br /> <br />
-        <input type="text" value={this.state.Description} name="Description" onChange={(data )=>{this.setState({Description:data.target.value}) }} /> <br /> <br />
-        <input type="date" value={this.state.Date_of_birth} name="Date_of_birth" onChange={(data )=>{this.setState({Date_of_birth:data.target.value}) }} /> <br /> <br />
+        <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        onClick={()=>{this.submit()}}>
+          <Form.Item label="Pid"></Form.Item>
+          <Input type="text" value={this.state.Pid} name="Pid" onChange={(data )=>{this.setState({Pid:data.target.value}) }} /> <br /> <br />
+          <Form.Item label="Email"></Form.Item>
+          <Input type="email" value={this.state.Email} name="Email" onChange={(data )=>{ this.setState({Email:data.target.value}) }} /> <br /> <br />
+          <Form.Item label="Dept_Code"></Form.Item>
+          <Input type="text" value={this.state.Dept_Code} name="Dept_code" onChange={(data )=>{this.setState({Dept_Code:data.target.value}) }} /> <br /> <br />
+          <Form.Item label="Course_Name"></Form.Item>
+          <Input type="text" value={this.state.Course_Name} name="Course_Name" onChange={(data )=>{this.setState({Course_Name:data.target.value}) }} /> <br /> <br />
+          <Form.Item label="CGPA"></Form.Item>
+          <Input type="number" step="0.01" value={this.state.CGPA} name="CGPA" onChange={(data )=>{this.setState({CGPA:data.target.value}) }} /> <br /> <br />
+          <Form.Item label="Semester"></Form.Item>
+          <Input type="number" value={this.state.Semester} name="Semester" onChange={(data )=>{this.setState({Semester:data.target.value}) }} /> <br /> <br />
+          <Form.Item label="Description"></Form.Item>
+          <Input type="text" value={this.state.Description} name="Description" onChange={(data )=>{this.setState({Description:data.target.value}) }} /> <br /> <br />
+          <Form.Item label="Date_of_birth"></Form.Item>
+          <Input type="date" value={this.state.Date_of_birth} name="Date_of_birth" onChange={(data )=>{this.setState({Date_of_birth:data.target.value}) }} /> <br /> <br />
 
-        <button onClick={()=>{this.submit()}} >Submit</button>
+          <Button type="primary" htmlType="submit" >Submit</Button>
+        </Form>
       </div>
     )
   }
 }
+
